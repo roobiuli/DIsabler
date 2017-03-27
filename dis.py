04f44a42e8507179d59e8c5b398d7230b3121673
Robert <nicoara.robert@gmail.com>
2017-03-28 01:59:38 +0300

    act_deact refactored

--- dis.py
+++ dis.py
@@ -4,6 +4,7 @@ import sys
 from subprocess import Popen, PIPE
 import re
 import argparse
+
 PAR = argparse.ArgumentParser(description="This script will activate / deactivate the \
                                                                   make_resolve_conf bash function which is \
                                                                    used by the DHCP Client / usually used \
@@ -47,9 +48,12 @@ def identifier(search):
     This function will search for bash function named make_resolv_conf()
     and it will gather line range info of declaration / content
     """
+    print "intr in func 3"
     com = "cat " + FILE  + " | grep -in" + " \"" + search +"\"" +" | cut -d':' -f1"
+    print com
     pos = bash(com)[0]
     spos = pos
+    print spos
     char = ""
     regex = re.compile("^(fi|#fi)")
     while not regex.match(char):
@@ -61,52 +65,60 @@ def identifier(search):
 def act_deact(*ar):
     try:
         numbs = identifier("is_on_read_only_partition /etc/resolv.")
+        print "."
     except ValueError:
         print "Bash Function not defined in dhclient hooks ... Inserting the function and activating"
         inserter()
         numbs = identifier("is_on_read_only_partition /etc/resolv.")
     element = {}
+    lst = []
     for line in range(int(numbs[0]), int(numbs[1])):
         command = "cat " + FILE + " | sed -e \"" + str(line) + "!" + "d" + "\""
         element[line] = bash(command)[0]
+        tmp = bash(command)[0]
+        lst.append({line, tmp})
     small = min(int(n) for n in element.keys())
-    for k, v in element.items():
-        if int(len(ar)) == 1:
-        #Setting activation commands( s= shell, s= small, r= replace, o= others, c= command )
-            ssc = "sed -i -e "  + "\'" + str(k) + "s/#//' " + FILE
-            src = "sed -i -e" + " \"" + str(k) + "s" + "/.*/    return/" + "\"" +" "+ FILE
-            soc = "sed -i -e" + " \"" + str(k) + "s" + "/" + str(v) + "/" +\
-                                                         str(v)[1:] + "/" + "\"" +" "+ FILE
-            act = "Activating..."
-        elif int(len(ar)) == 2:
-        #Setting DEactivation commands( s= shell, s= small, r= replace, o= others, c= command )
-            ssc = "sed -i -e "  + "\'" + str(k) + "s" + "/if \[/" + "#if \[/' " + FILE
-            src = "sed -i -e" + " \"" + str(k) + "s" + "/.*/    #Deactivated by CSRU/" + \
-                                                                                                         "\"" + " " + FILE
-            soc = cmd = "sed -i -e "  + "'" + str(k) + "s" + "/" + str(v) + "/" + "#" + \
-                                                                         str(v) + "/'"  +" " + FILE
-            act = "Deactivating..."
-    all = [small, ssc, src, soc, act]
+    print element
+    print lst
+    if len(lst) != 3:
+        print "If statment that controles function has more then 3 lines"
+        sys.exit(1)
+    if int(len(ar)) == 2:
+    #Setting activation commands( s= shell, s= small, r= replace, o= others, c= command )
+        ssc = "sed -i -e "  + "\'" + str(lst[0].keys()) + "s/#//' " + FILE
+        src = "sed -i -e" + " \"" + str(lst[1].keys()) + "s" + "/.*/    return/" + "\"" +" "+ FILE
+        soc = "sed -i -e" + " \"" + str(lst[2].keys()) + "s" + "/" + str(lst[2].values()) + "/" +\
+                                                        str(lst[2].values())[1:] + "/" + "\"" +" "+ FILE
+        act = "Activating..."
+    elif int(len(ar)) == 1:
+    #Setting DEactivation commands( s= shell, s= small, r= replace, o= others, c= command )
+        ssc = "sed -i -e "  + "\'" + str(lst[0].keys()) + "s" + "/if \[/" + "#if \[/' " + FILE
+        src = "sed -i -e" + " \"" + str(lst[1].keys()) + "s" + "/.*/    #Deactivated by CSRU/" + \
+                                                                                "\"" + " " + FILE
+        soc = cmd = "sed -i -e "  + "'" + str(lst[2].keys()) + "s" + "/" + str(lst[2].values()) + "/" + "#" + \
+                                                                        str(lst[2].values()) + "/'"  +" " + FILE
+        act = "Deactivating..."
+    all = [small, ssc, src, soc, act, element]
     return all
 
 
-def main(*ar):
+def main():
     if args.A is True:
-      holder = act_deact("-")
+        holder = act_deact("-")
     elif args.D is True:
-      holder = act_deact("-", "-")
+        holder = act_deact("-", "-")
+    element = holder[5]
     print holder[4]
     for k, v  in element.items():
         if k == int(holder[0]):
-            cmd =  holder[1]
-
+            cmd = holder[1]
             #### COntinue FORM HERE (HOME)
             print "executin {}".format(cmd)
         elif "#Deactivated by CSRU" in v or "return" in v:
-            cmd = src
+            cmd = holder[2]
             print "executin {}".format(cmd)
         else:
-            cmd = soc
+            cmd = holder[3]
             print "executin {}".format(cmd)
         ret = bash(cmd)[2]
         if ret != 0:
